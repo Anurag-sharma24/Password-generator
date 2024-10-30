a6e8f73c50a5c1a34f9021eb7ea25c32c71890e2
Anurag sharma24 <annu12241999@gmail.com>
2024-10-30 16:58:33 +0530

    password generator

---
+++ script.js
@@ -0,0 +1,24 @@
+const passwordBox = document.getElementById("password")
+const length = 10;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const symbol = "@#$%^&*()_+~{}[]<>-=";
+const number = "012456789";
+const allChars = upperCase + lowerCase + symbol + number;
+
+function generatePassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
